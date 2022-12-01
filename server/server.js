b2674bf949e991bf002fc46af57d619d5dfe980c
Anurag Verma <anurag.premaverma@gmail.com>
2022-12-01 10:18:29 +0530

    server is created

--- server/server.js
+++ server/server.js
@@ -1,30 +1,29 @@
-import  express  from "express";
-import mongoose from 'mongoose';
-import cors  from 'cors'
 import bodyParser from "body-parser";
-const PORT = 4000;
-const  app = express();
-app.use(cors())
-app.use(bodyParser.json())
-// / instead of then you can use await 
-await mongoose.connect("mongodb+srv://anurag_premaverma:anurag.prema@cluster1.85y2dfr.mongodb.net/test")
-console.log("MongoDB connection is successfull")
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+import passport from "passport";
+import passportConfig from "./config/passport.js";
+import connect from "./database/mongdb.js";
+import routes from "./routes/index.js";
 
-// .then(()=>console.log("Mongo is running successfull"))
-// .catch((err)=>{
-//     console.error(err)
-// })
+dotenv.config();
 
-app.get("/", (req, res)=>{
-    res.send("Hello world");
-})
+const PORT = process.env.PORT || 4000;
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(passport.initialize());
+passportConfig(passport);
 
-app.post("/transaction", (req, res)=>{
-    const {amount, description, date} = req.body;
-    res.json({message: "hello world"});
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/", routes);
 
+await connect();
 
-app.listen(PORT, ()=>{
-    console.error(`server is running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log("Server is running at http://localhost:4000");
+});
+~
